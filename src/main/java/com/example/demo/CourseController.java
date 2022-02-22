875bdb1e5a5031c647beb159b39f4fb50539d69a
Aliakbar <example.com>
2022-02-22 12:35:38 +0530

    error handled for negative ID

--- src/main/java/com/example/demo/CourseController.java
+++ src/main/java/com/example/demo/CourseController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import java.lang.String;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 public class CourseController{
@@ -30,10 +33,14 @@ public class CourseController{
     public Course getCourseDetails(@PathVariable long id) {
 
         Optional<Course> course = repository.findById(id);
-        if (course.isPresent())
+
+       if(id<=0)
+            throw new RuntimeException("Invalid address, please enter positive integer");
+        else if (course.isPresent())
             return course.get();
         else
         throw new RuntimeException("Course not found with id " + id);
+
     }
 
 
@@ -49,6 +56,8 @@ public class CourseController{
     //PUT - Update/Replace a resource (/courses/1)
     @PutMapping("/courses/{id}")
     public void updateCourse(@PathVariable long id, @RequestBody Course course) {
+        if(id<=0)
+            throw new RuntimeException("Invalid address, please enter positive integer");
         try {
             if (getCourseDetails(id) == null)
                 throw new RuntimeException("Course not found with id " + id);
@@ -62,8 +71,10 @@ public class CourseController{
     //DELETE - Delete a resource (/courses/1)
     @DeleteMapping("/courses/{id}")
     public void deleteCourse(@PathVariable long id) {
+        if(id<=0)
+            throw new RuntimeException("Invalid address, please enter positive integer");
         try {
-            if (getCourseDetails(id) == null)
+            if (getCourseDetails(id) == null && id<0)
                 throw new RuntimeException("Course not found with id " + id);
         }
 
